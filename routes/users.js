632e731fc9003247802d626d7c944f2560fe633d
fclmrj <fclmrj@msn.com>
2018-10-17 16:12:09 -0300

    Adicionado app gerado com ExpressGenerator Configurado integração do AngularJS com NodeJs

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var jogos = [
+   {codigo:1 , nome:'PES 2K1X' , preco: 19.46 , categoria: 'Aventura'},
+   {codigo:2 , nome:'Lego Jurassic Word' , preco: 21.68 , categoria: 'Ação'},
+   {codigo:3 , nome:'Spore'              , preco: 54.63 , categoria: 'Survival'}
+];
+
+/* GET users listing. */
+router.get('/softwares', function(req, res, next){
+    res.write(JSON.stringify(jogos));
+    console.log('+Request GET solicitada');
+    res.end();
+});
+
+module.exports = router;
