49761dd9e0ef7796f2d11b22e7dcd8364a04ebe2
fclmrj <fclmrj@msn.com>
2018-10-20 12:00:05 -0300

    Alterada descrição do JSON de Softwares

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var jogos = [
-   {codigo:1 , nome:'PES 2K1X' , preco: 19.46 , categoria: 'Aventura'},
-   {codigo:2 , nome:'Lego Jurassic Word' , preco: 21.68 , categoria: 'Ação'},
-   {codigo:3 , nome:'Spore'              , preco: 54.63 , categoria: 'Survival'}
+var softwares = [
+   {codigo:1 , nome:"PES 2K1X" , preco: 19.46 , categoria: "Aventura"},
+   {codigo:2 , nome:"Lego Jurassic Word" , preco: 21.68 , categoria: "Ação"},
+   {codigo:3 , nome:"Spore"              , preco: 54.63 , categoria: "Survival"}
 ];
 
 /* GET users listing. */
-router.get('/softwares', function(req, res, next){
-    res.write(JSON.stringify(jogos));
-    console.log('+Request GET solicitada');
+router.get("/softwares", function(req, res, next){
+    res.write(JSON.stringify(softwares));
+    console.log("+Request GET solicitada");
     res.end();
 });
 
